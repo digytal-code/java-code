54eeaedb896051bc505fd12f9ded796aa4c17925
Gleyson Sampaio <glysns@hotmail.com>
2022-01-11 11:00:34 -0300

    formulario sessao

---
+++ components/desktop-util/src/main/java/code/desktop/desktop/FormularioSessao.java
@@ -0,0 +1,63 @@
+package code.desktop.desktop;
+
+import java.time.LocalDateTime;
+
+public class FormularioSessao {
+	private LocalDateTime dataHora=LocalDateTime.now();
+	private String login;
+	private String nome;
+	private String cpf;
+	private String empresa;
+	private String cnpj;
+	private Integer idUsuario;
+	private Integer idEmpresa;
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public Integer getIdEmpresa() {
+		return idEmpresa;
+	}
+	public void setIdEmpresa(Integer idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+	
+}
