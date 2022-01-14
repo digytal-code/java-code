faf4303ff4e3fb08a4940c34871968d9385bdbf8
Gleyson Sampaio <glysns@hotmail.com>
2022-01-14 17:47:49 -0300

    esocial

---
+++ projects/mensageria-esocial/src/main/java/digytal/esocial/MensageriaEsocialApplication.java
@@ -0,0 +1,13 @@
+package digytal.esocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageriaEsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageriaEsocialApplication.class, args);
+	}
+
+}
---
+++ projects/mensageria-esocial/src/test/java/digytal/esocial/MensageriaEsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package digytal.esocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaEsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
