e6b6a5f7d8bfc29269937a242c2b6b4c347a1b7d
Gleyson Sampaio <glysns@hotmail.com>
2022-03-30 21:42:54 -0300

    movimentacoes

--- projects/spring-boot-rest-api/src/main/java/digytal/springboot/restapi/controller/MovimentacaoController.java
+++ projects/spring-boot-rest-api/src/main/java/digytal/springboot/restapi/controller/MovimentacaoController.java
@@ -1,10 +1,36 @@
 package digytal.springboot.restapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import digytal.springboot.restapi.dto.NovaMovimentacao;
+import digytal.springboot.restapi.model.Correntista;
+import digytal.springboot.restapi.model.Movimentacao;
+import digytal.springboot.restapi.repository.MovimentacaoRepository;
+import digytal.springboot.restapi.service.MovimentacaoService;
 
 @RestController
 @RequestMapping("/movimentacoes")
 public class MovimentacaoController {
+	@Autowired
+	private MovimentacaoService service;
+	
+	@Autowired
+	private MovimentacaoRepository repository;
 
+	@PostMapping
+	public void gravar(@RequestBody NovaMovimentacao movimentacao) {
+		service.gravar(movimentacao);
+	}
+	@GetMapping("/{idConta}")
+    public List<Movimentacao> listarMovimentacoes(@PathVariable("idConta") Integer idConta){
+    		return repository.findByIdConta(idConta);
+    }
 }
--- projects/spring-boot-rest-api/src/main/java/digytal/springboot/restapi/repository/MovimentacaoRepository.java
+++ projects/spring-boot-rest-api/src/main/java/digytal/springboot/restapi/repository/MovimentacaoRepository.java
@@ -1,8 +1,11 @@
 package digytal.springboot.restapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import digytal.springboot.restapi.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
-    
+    public List<Movimentacao> findByIdConta(Integer idConta);
 }
