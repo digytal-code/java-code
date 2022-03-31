fde0837f46cd97bb11cfe34562e4cefe017865bb
Gleyson Sampaio <glysns@hotmail.com>
2022-03-30 21:35:59 -0300

    movimentacao

---
+++ projects/spring-boot-rest-api/src/main/java/digytal/springboot/restapi/dto/NovaMovimentacao.java
@@ -0,0 +1,35 @@
+package digytal.springboot.restapi.dto;
+
+import digytal.springboot.restapi.model.TipoMovimentacao;
+
+public class NovaMovimentacao {
+	private Integer idConta;
+	private String descricao;
+	private Double valor;
+	private TipoMovimentacao tipo;
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public TipoMovimentacao getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoMovimentacao tipo) {
+		this.tipo = tipo;
+	}
+	
+}
--- projects/spring-boot-rest-api/src/main/java/digytal/springboot/restapi/service/MovimentacaoService.java
+++ projects/spring-boot-rest-api/src/main/java/digytal/springboot/restapi/service/MovimentacaoService.java
@@ -1,12 +1,45 @@
 package digytal.springboot.restapi.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import digytal.springboot.restapi.dto.NovaMovimentacao;
+import digytal.springboot.restapi.model.Correntista;
+import digytal.springboot.restapi.model.Movimentacao;
+import digytal.springboot.restapi.model.TipoMovimentacao;
+import digytal.springboot.restapi.repository.CorrentistaRepository;
 import digytal.springboot.restapi.repository.MovimentacaoRepository;
 
 @Service
 public class MovimentacaoService {
 	@Autowired
 	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
+	
+	
+	@Transactional
+	public void gravar(NovaMovimentacao novaMovimentacao){
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+		if(correntista!=null) {
+			Double valor = novaMovimentacao.getValor();
+			TipoMovimentacao tipo = novaMovimentacao.getTipo();
+			valor = tipo==TipoMovimentacao.CREDITO? valor : valor * -1;
+			Movimentacao movimentacao = new Movimentacao();
+			movimentacao.setDataHora(LocalDateTime.now());
+			movimentacao.setIdConta(novaMovimentacao.getIdConta());
+			movimentacao.setDescricao(novaMovimentacao.getDescricao());
+			movimentacao.setValor(novaMovimentacao.getValor());
+			movimentacao.setTipo(tipo);
+			
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+			repository.save(movimentacao);
+			correntistaRepository.save(correntista);
+		}
+	
+	}
 }
